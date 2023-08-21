5b2ba6c4272c51e56320c795731653a4508cace8
Luigi Bros <luigigiampaoli1@gmail.com>
2023-08-21 18:52:49 -0300

    Mudanças feitas nos aruqivos Java e Python

--- Pasta_de_Estudos/Java/Entretainment/Computer.java
+++ Pasta_de_Estudos/Java/Entretainment/Computer.java
@@ -9,7 +9,7 @@ public class Computer<o> extends Player {
 
     public Computer(String nome) {
         super(nome);
-        this.nome = "Computador";
+        nome = "Computador";
         while (forca + velocidade > 20) {
             this.velocidade = valor.nextInt(20);
             this.forca = valor.nextInt(20);
--- Pasta_de_Estudos/Python/Game.py
+++ Pasta_de_Estudos/Python/Game.py
@@ -1,82 +1,32 @@
-import time
-
-def start_game():
-    print("You wake up in your room. It's dark and quiet. You realize you need to escape from your house.")
-    time.sleep(2)
-    print("You can see a window, a door, and a staircase.")
-    time.sleep(1)
-    print("What do you do?")
-    time.sleep(1)
-    print("1. Go to the window")
-    print("2. Go to the door")
-    print("3. Go to the staircase")
-    choice = input("Enter your choice (1, 2, or 3): ")
-
-    if choice == "1":
-        window()
-    elif choice == "2":
-        door()
-    elif choice == "3":
-        staircase()
+while (True):
+    primeiraprova = int(input("\nQual foi a nota da PRIMEIRA PROVA? "))
+    if (primeiraprova < 0 or primeiraprova > 100):
+        print("Nota recusada, coloque outra!!!")
     else:
-        print("Invalid choice. Please try again.")
-        start_game()
+        primeiraprova *= 0.2
+        break
 
-def window():
-    print("You approach the window. It's locked, but you notice a key on the table nearby.")
-    time.sleep(1)
-    print("What do you do?")
-    time.sleep(1)
-    print("1. Take the key and unlock the window")
-    print("2. Go back")
-    choice = input("Enter your choice (1 or 2): ")
-
-    if choice == "1":
-        print("You unlock the window and climb out. Congratulations, you escaped!")
-    elif choice == "2":
-        start_game()
+while (True):
+    segundaprova = int(input("\nQual foi a nota da SEGUNDA PROVA? "))
+    if (segundaprova < 0 or segundaprova > 100):
+        print("Nota recusada, coloque outra!!!")
     else:
-        print("Invalid choice. Please try again.")
-        window()
+        segundaprova *= 0.3
+        break
 
-def door():
-    print("You try to open the door, but it's locked. You need a key to unlock it.")
-    time.sleep(1)
-    print("What do you do?")
-    time.sleep(1)
-    print("1. Look for the key")
-    print("2. Go back")
-    choice = input("Enter your choice (1 or 2): ")
-
-    if choice == "1":
-        print("You search around the room and find the key hidden under your bed.")
-        time.sleep(1)
-        print("You take the key and unlock the door. Congratulations, you escaped!")
-    elif choice == "2":
-        start_game()
+while (True):    
+    provaobjetiva = int(input("\nQual foi a nota da PROVA OBJETIVA? "))
+    if (provaobjetiva < 0 or provaobjetiva > 100):
+        print("Nota recusada, coloque outra!!!")
     else:
-        print("Invalid choice. Please try again.")
-        door()
+        provaobjetiva *= 0.5
+        break
 
-def staircase():
-    print("You head towards the staircase, but you hear a strange noise coming from downstairs.")
-    time.sleep(1)
-    print("What do you do?")
-    time.sleep(1)
-    print("1. Go downstairs to investigate")
-    print("2. Go back")
-    choice = input("Enter your choice (1 or 2): ")
+media = (primeiraprova+segundaprova+provaobjetiva)
 
-    if choice == "1":
-        print("You cautiously go downstairs, but the noise gets louder and scarier.")
-        time.sleep(1)
-        print("Suddenly, a ghostly figure appears in front of you. You scream and run back to your room.")
-        time.sleep(1)
-        print("Game Over!")
-    elif choice == "2":
-        start_game()
-    else:
-        print("Invalid choice. Please try again.")
-        staircase()
-
-start_game()
+if(media < 30):
+    print("Reprovado!!!")
+elif(media < 70 and media > 30):
+    print("Precisará fazer o exame!!!")
+else:
+    print("Aprovado!!!")
